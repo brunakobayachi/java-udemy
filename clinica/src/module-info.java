e114c1cd1f80444e076dcbe20dde81c07e99dadd
brunakobayachi <brunakobayachi@gmail.com>
2022-10-18 16:43:26 -0300

    feat aula1

---
+++ clinica/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author bruna
+ *
+ */
+module clinica {
+}
---
+++ exercicios/src/fundamentos/Temperatura.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class Temperatura {
+	public static void main(String[] args) {
+		final int constanteF = 32;
+		final double divisaoF = 5.0/9.0;
+		double tempF = 82;
+		double tempC = divisaoF * (tempF - constanteF);
+		
+		System.out.println("A temperatura convertida r: " + tempC + "C");
+		
+		
+		
+		
+	}
+}
